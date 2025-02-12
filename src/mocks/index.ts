8787810d0c518dab5e1feb856de445e7d3c34aa9
Glendon Philipp Baculio <gpbaculio@gmail.com>
2025-02-12 14:11:57 +0800

    add mock api

---
+++ src/mocks/index.ts
@@ -0,0 +1,13 @@
+export * from './api';
+
+// Example usage
+// const api = new ProductsShopAPI();
+
+// async function example() {
+//   const data = await api.getProducts({
+//     page: 1,
+//     category: 'Clothing',
+//   });
+//   console.log(data);
+// }
+// example();
