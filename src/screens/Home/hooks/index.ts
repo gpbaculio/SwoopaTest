8ad675dde2f665246d93c374f220c2e8813d2a0d
Glendon Philipp Baculio <gpbaculio@gmail.com>
2025-02-12 16:49:01 +0800

    use infinite hook

--- src/screens/Home/hooks/index.ts
+++ src/screens/Home/hooks/index.ts
@@ -1,62 +1,37 @@
-import {keepPreviousData, useQuery} from '@tanstack/react-query';
-import {useState} from 'react';
-
+import {InfiniteData, useSuspenseInfiniteQuery} from '@tanstack/react-query';
 import {PaginatedResponse, Product, ProductsShopAPI} from 'src/mocks';
 
-interface UsePaginatedQueryOptions {
-  category?: Product['category'];
+interface UseInfiniteProductsOptions {
+  category?: Product['category'] | 'All';
 }
 
-type ProductsQueryKey = [
-  'products',
-  {page: number; category?: Product['category']},
-];
+type ProductsQueryKey = ['products', {category?: Product['category'] | 'All'}];
 
-export function usePaginatedQuery({category}: UsePaginatedQueryOptions) {
-  const [page, setPage] = useState(1);
+export function useInfiniteProductsQuery({
+  category,
+}: UseInfiniteProductsOptions) {
+  const api = new ProductsShopAPI();
 
-  const {data, isLoading, isError} = useQuery<
+  const {data, fetchNextPage, hasNextPage} = useSuspenseInfiniteQuery<
     PaginatedResponse<Product>,
     Error,
-    PaginatedResponse<Product>,
-    ProductsQueryKey
+    InfiniteData<PaginatedResponse<Product>>,
+    ProductsQueryKey,
+    number
   >({
-    queryKey: ['products', {page, category}],
-    queryFn: async ({queryKey}) => {
-      const [_, {page: queryPage, category: queryCategory}] = queryKey;
-      const api = new ProductsShopAPI();
-      return api.getProducts({page: queryPage, category: queryCategory});
+    queryKey: ['products', {category}],
+    queryFn: async ({pageParam = 1}) =>
+      api.getProducts({category, page: pageParam}),
+    initialPageParam: 1,
+    getNextPageParam: lastPage => {
+      const nextPage = lastPage.pagination.currentPage + 1;
+      return nextPage <= lastPage.pagination.totalPages ? nextPage : undefined;
     },
-    placeholderData: keepPreviousData,
   });
 
-  const goToNextPage = () => {
-    if (
-      data?.pagination?.currentPage &&
-      data?.pagination?.currentPage < data?.pagination?.totalPages
-    ) {
-      setPage(prev => prev + 1);
-    }
-  };
-
-  const goToPreviousPage = () => {
-    if (data?.pagination?.currentPage && data?.pagination?.currentPage > 1) {
-      setPage(prev => prev - 1);
-    }
-  };
-
   return {
-    products: data?.data ?? [],
-    pagination: data?.pagination ?? {
-      currentPage: page,
-      totalPages: 1,
-      totalItems: 0,
-      itemsPerPage: 10,
-    },
-    isLoading,
-    isError,
-    goToNextPage,
-    goToPreviousPage,
-    setPage,
+    products: data?.pages.flatMap(page => page.data) ?? [],
+    fetchNextPage,
+    hasNextPage,
   };
 }
